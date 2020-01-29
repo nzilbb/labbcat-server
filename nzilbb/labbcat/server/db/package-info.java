598c53c2beb487e8337823ececaab1bb2342f314
Robert Fromont <robert@fromont.net.nz>
2020-01-29 11:15:56 -0300

    Add JavaDoc support files.

---
+++ nzilbb/labbcat/server/db/package-info.java
@@ -0,0 +1,26 @@
+//
+// Copyright 2020 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of LaBB-CAT.
+//
+//    LaBB-CAT is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    LaBB-CAT is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with LaBB-CAT; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+/**
+ * Graph Store implementations and supporting classes, which use an RDBMS database
+ * (assumed to by MySQL) as an annotation store, and the file system as a file store.
+ */
+package nzilbb.labbcat.server.db;
