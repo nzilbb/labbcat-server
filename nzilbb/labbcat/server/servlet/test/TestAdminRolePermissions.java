427bc39a3e08a5ea00b46a3e47294ba5ae4302cf
Robert Fromont <robert@fromont.net.nz>
2020-06-24 20:15:17 -0300

    Check only permissions for selected role are returned.

--- nzilbb/labbcat/server/servlet/test/TestAdminRolePermissions.java
+++ nzilbb/labbcat/server/servlet/test/TestAdminRolePermissions.java
@@ -242,10 +242,11 @@ public class TestAdminRolePermissions
          assertTrue("There's at least one rolePermission", rolePermissions.length >= 1);
          boolean found = false;
          for (RolePermission c : rolePermissions) {
+            assertEquals("Only correct role listed",
+                         originalRolePermission.getRoleId(), c.getRoleId());
             if (c.getRoleId().equals(originalRolePermission.getRoleId())
                 && c.getEntity().equals(originalRolePermission.getEntity())) {
                found = true;
-               break;
             }
          }
          assertTrue("RolePermission was added", found);
