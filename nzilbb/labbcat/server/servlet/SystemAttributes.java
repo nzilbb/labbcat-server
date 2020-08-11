789cfa749bb637e0263830378a9c6a55f21389f9
Robert Fromont <robert@fromont.net.nz>
2020-08-11 18:10:14 -0300

    Add api/systemattributes servlet

---
+++ nzilbb/labbcat/server/servlet/SystemAttributes.java
@@ -0,0 +1,52 @@
+//
+// Copyright 2020 New Zealand Institute of Language, Brain and Behaviour, 
+// University of Canterbury
+// Written by Robert Fromont - robert.fromont@canterbury.ac.nz
+//
+//    This file is part of LaBB-CAT.
+//
+//    LaBB-CAT is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    LaBB-CAT is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with LaBB-CAT; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+package nzilbb.labbcat.server.servlet;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Vector;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+/**
+ * Servlet that allows read-only access to system attributes.
+ * <p> See <a href="package-summary.html#/api/admin/corpora">API summary</a> for more details.
+ * @author Robert Fromont robert@fromont.net.nz
+ */
+@WebServlet(urlPatterns = "/api/systemattributes/*", loadOnStartup = 20)
+public class SystemAttributes extends TableServletBase {   
+   
+   public SystemAttributes() {
+      super("system_attribute", // table
+            new Vector<String>() {{ // primary keys
+               add("name");
+            }},
+            new Vector<String>() {{ // columns
+               add("value");
+            }},
+            "name"); // order
+   }
+   
+   private static final long serialVersionUID = 1;
+} // end of class SystemAttributes
