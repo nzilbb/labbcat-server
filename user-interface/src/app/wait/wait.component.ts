573eb3206aad8d48604e2b7505aab9d9b3b47180
Robert Fromont <robert@fromont.net.nz>
2020-08-11 18:11:18 -0300

    Ensure wait animation works in both dev and prod environments.

--- user-interface/src/app/wait/wait.component.ts
+++ user-interface/src/app/wait/wait.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-wait',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaitComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    imagesLocation = environment.imagesLocation;
+    constructor() { }
+    
+    ngOnInit(): void {
+    }
+    
 }
