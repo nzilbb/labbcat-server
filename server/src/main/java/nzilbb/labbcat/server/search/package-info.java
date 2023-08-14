7997ca6b52b3cbef571109c7308e6ef9cf8aadee
Robert Fromont <robert@fromont.net.nz>
2023-08-14 14:22:25 -0300

    Document matrix JSON encoding.

--- server/src/main/java/nzilbb/labbcat/server/search/package-info.java
+++ server/src/main/java/nzilbb/labbcat/server/search/package-info.java
@@ -21,5 +21,80 @@
 //
 /**
  * Classes that manage definition, serializations, and deserialization of search matrices.
+ *
+ * <p> A search matrix is generally encoded as a JSON object, with the following structure:
+ * <dl>
+ *   <dt>columns</dt>
+ *   <dd> An array of JSON objects, each representing a column (a word token) in the
+ *        search matrix UI. Each object as the following structure:
+ *        <dl>
+ *          <dt>layers</dt>
+ *          <dd> A JSON object where keys are layer IDs, and values are JSON objects with
+ *               the following structure:
+ *                <dl>
+ *                 <dt>pattern</dt> <dd>A regular expression to match the label.</dd>
+ *                 <dt>not</dt> <dd>Whether pattern matching is negated or not.</dd>
+ *                 <dt>min</dt> <dd>Inclusive numeric minimum value for the label.</dd>
+ *                 <dt>max</dt> <dd>Exclusive numeric maximum value for the label.</dd>
+ *                 <dt>anchorStart</dt> <dd>Whether this annotation starts with the word.</dd>
+ *                 <dt>anchorEnd</dt> <dd>Whether this annotation ends with the word.</dd>
+ *                 <dt>target</dt> <dd>Whether this is the target annotation of the search.</dd>
+ *                </dl>
+ *          </dd>
+ *          <dt>adj</dt>
+ *          <dd> How many word tokens away from this token the token that matches the next
+ *               column can be. </dd>
+ *        </dl>
+ *   </dd>
+ *
+ *   <dt>participantQuery</dt>
+ *   <dd>An optional query to identify participants whose utterances should be searched.</dd>
+ *
+ *   <dt>transcriptQuery</dt>
+ *   <dd>An optional to identify transcripts whose utterances should be searched.</dd>
+ * </dl>
+ *
+ * <p> Any match condition in a column can contain no conditions - i.e. no pattern, min,
+ * or max. In this case, the condition is ignored.
+ *
+ * <p> e.g. the following JSON-encoded search matrix identifies all tokens of the word
+ * "the" at the beginning of a topix, followed withing three words by a token that starts
+ * with a vowel in the pronunciation: 
+ * <pre>{
+ *   "columns":[
+ *     {
+ *       "layers":{
+ *         "orthography":{
+ *           "id":"orthography",
+ *           "not":false,
+ *           "pattern":"and",
+ *           "target":true},
+ *         "phonemes":{
+ *           "id":"phonemes",
+ *           "target":false},
+ *         "topic":{
+ *           "id":"topic",
+ *           "target":false,
+ *           "anchorStart":true}
+ *       },
+ *       "adj":3
+ *     },
+ *     {
+ *       "layers":{
+ *         "orthography":{
+ *           "id":"orthography",
+ *           "target":false},
+ *         "phonemes":{
+ *           "id":"phonemes",
+ *           "target":false,
+ *           "pattern":"[aeiou].*"},
+ *         "topic":{
+ *           "id":"topic",
+ *           "target":false
+ *         }
+ *       }
+ *     }]
+ * }</pre>
+ *
  */
 package nzilbb.labbcat.server.search;
