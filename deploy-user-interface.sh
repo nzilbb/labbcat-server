534e02cc3f79775aa4cb39aaa8354e0a85cf374a
Robert Fromont <robert@fromont.net.nz>
2024-04-18 17:07:07 -0300

    Tidy up automated packaging of user-interface.

--- deploy-user-interface.sh
+++ deploy-user-interface.sh
@@ -3,23 +3,23 @@
 # which is install in the location specified by the first parameter, or:
 LOCAL_LABBCAT=${1:-/var/lib/tomcat9/webapps/labbcat}
 
-if (mvn package -pl :nzilbb.labbcat.user-interface)
+if (mvn clean package -pl :nzilbb.labbcat.user-interface)
 then
     if [ ! -d $LOCAL_LABBCAT/user-interface ]; then
         mkdir $LOCAL_LABBCAT/user-interface;
     fi
     rm -rf $LOCAL_LABBCAT/user-interface/*
-    cp -r user-interface/target/labbcat-view/* $LOCAL_LABBCAT/user-interface/
+    cp -r user-interface/target/labbcat-view/browser/* $LOCAL_LABBCAT/user-interface/
     
     if [ ! -d $LOCAL_LABBCAT/edit/user-interface ]; then
         mkdir -p $LOCAL_LABBCAT/edit/user-interface;
     fi
     rm -rf $LOCAL_LABBCAT/edit/user-interface/*
-    cp -r user-interface/target/labbcat-edit/* $LOCAL_LABBCAT/edit/user-interface/
+    cp -r user-interface/target/labbcat-edit/browser/* $LOCAL_LABBCAT/edit/user-interface/
     
     if [ ! -d $LOCAL_LABBCAT/admin/user-interface ]; then
         mkdir -p $LOCAL_LABBCAT/admin/user-interface;
     fi
     rm -rf $LOCAL_LABBCAT/admin/user-interface/*
-    cp -r user-interface/target/labbcat-admin/* $LOCAL_LABBCAT/admin/user-interface/
+    cp -r user-interface/target/labbcat-admin/browser/* $LOCAL_LABBCAT/admin/user-interface/
 fi
