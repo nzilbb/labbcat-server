0414bc489a80e13cfac555087f20a2e9f43bc3ba
Robert Fromont <robert@fromont.net.nz>
2024-06-18 16:53:04 -0300

    Keep angular URLs synchronized with help page URL.

--- user-interface/src/main/angular/projects/labbcat-view/src/app/app.component.ts
+++ user-interface/src/main/angular/projects/labbcat-view/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { environment } from '../environments/environment';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,19 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent {
     // TODO for now the production environment uses individual components hosted inside
-    // TODO the classic, and so we don't want heading/footer, because the host page
+    // TODO the classic, and so we don't want header/footer, because the host page
     // TODO already has them 
     title = "LaBB-CAT";
     production = environment.production;
+    constructor(private router: Router) {
+        router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                // publish page changes, so the page header can update help links 
+                window.postMessage({
+                    app: "labbcat-view",
+                    url: event.url
+                });
+            }
+        });
+    }
 }
