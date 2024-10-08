b42c15e953014b5e619487176a7fad5f674e0b1e
Robert Fromont <robert@fromont.net.nz>
2024-10-08 15:35:09 -0300

    Ensure slow-loading page headings get registered as the document title.

--- user-interface/src/main/angular/projects/labbcat-view/src/app/app.component.ts
+++ user-interface/src/main/angular/projects/labbcat-view/src/app/app.component.ts
@@ -28,12 +28,21 @@ export class AppComponent {
                     url: event.url
                 });
                 const h1 = document.getElementById("title");
-                if (h1) {
+                if (h1 && h1.textContent) {
                     this.setPageTitle(h1.textContent);
                 } else { // no title element, so use URL
                     this.setPageTitle(event.url
                         .replace(/^\//,"") // strip initial slash
-                        .replace(/\//g," » ")); // transform remaining slashes
+                        .replace(/\//g," » ") // transform remaining slashes
+                        .replace(/[\?#].*/,"")); // strip off request parameters
+                    if (h1) { // there is an element, it might just not have been populated yet
+                        // wait half a sec and try again
+                        setTimeout(()=>{
+                            if (h1.textContent) {
+                                this.setPageTitle(h1.textContent);
+                            }
+                        }, 500);
+                    }
                 }
             }
         });
