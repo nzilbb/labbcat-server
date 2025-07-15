5700a4fe4994ea510a7c401b723cc7e9bdf8a7f0
Robert Fromont <robert@fromont.nz>
2025-07-15 10:56:07 -0300

    Add support for specifying CSV target column when re-loading results.

--- user-interface/src/main/angular/projects/labbcat-view/src/app/matches-upload/matches-upload.component.ts
+++ user-interface/src/main/angular/projects/labbcat-view/src/app/matches-upload/matches-upload.component.ts
@@ -11,6 +11,11 @@ import { Layer, Task } from 'labbcat-common';
 export class MatchesUploadComponent implements OnInit {
 
     csv: File;
+    rowCount: number;
+    headers: string[];
+    csvFieldDelimiter: string
+    targetColumn: string;
+
     threadId: string;
     task: Task;
     
@@ -30,6 +35,43 @@ export class MatchesUploadComponent implements OnInit {
             this.csv = null;
             return;
         }
+
+        // read the file to determine columns and row count
+        const reader = new FileReader();
+        const component = this;
+        reader.onload = () => {  
+            const csvData = reader.result;  
+            let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);
+            // remove blank lines
+            csvRecordsArray = csvRecordsArray.filter(l=>l.length>0);
+            if (csvRecordsArray.length == 0) {
+                component.messageService.error("File is empty: " + component.csv.name);
+            } else {
+                this.rowCount = csvRecordsArray.length - 1; // (don't count header line)
+                
+                // get headers...
+                const firstLine = csvRecordsArray[0];
+                // split the line into fields
+                this.csvFieldDelimiter = ",";
+                if (firstLine.match(/.*\t.*/)) this.csvFieldDelimiter = "\t";
+                else if (firstLine.match(/.;.*/)) this.csvFieldDelimiter = ";";
+                const fields = firstLine.split(this.csvFieldDelimiter);
+                // the fields might be quoted, so remove quotes
+                this.headers = fields.map(f=>f.replace(/^"(.*)"$/g, "$1"))
+
+                // set default column selection...
+                if (this.headers.includes("MatchId")) {
+                    this.targetColumn = "MatchId";
+                } else if (this.headers.includes("URL")) {
+                    this.targetColumn = "URL";
+                }                
+            }
+        };
+        reader.onerror = function () {  
+            component.messageService.error("Error reading " + component.csv.name);
+        };
+        reader.readAsText(this.csv);
+        
     }
     
     processing = false;
@@ -40,7 +82,7 @@ export class MatchesUploadComponent implements OnInit {
         this.processingError = "";
         this.threadId = null;
         this.labbcatService.labbcat.resultsUpload(
-            this.csv, "MatchId"/*TODO*/, (response, errors, messages) => {
+            this.csv, this.targetColumn, (response, errors, messages) => {
                 
                 if (errors) errors.forEach(m => this.messageService.error(m));
                 if (messages) messages.forEach(m => this.messageService.info(m));
@@ -62,7 +104,7 @@ export class MatchesUploadComponent implements OnInit {
     finished(task: Task): void {
         this.processing = false;
         if (task.resultUrl) {
-            document.location = task.resultUrl;
+            document.location = `matches?threadId=${task.threadId}`; //TODO task.resultUrl;
         }
     }
 }
